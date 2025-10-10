a6a9139a5e05a5939c3ec9dd2ca31e076486b683
RichardTaku <145557486+RichardTaku@users.noreply.github.com>
2025-10-10 10:32:19 +0100

    maven work

--- src/main/java/com/theedesigner/controller/TaskController.java
+++ src/main/java/com/theedesigner/controller/TaskController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TaskController {
 
-    @GetMapping("/")
+    @GetMapping({"/","/microserviceapp"})
     public String homePage() {
         return """
                 <html>
